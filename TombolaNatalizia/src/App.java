c081a6a25d72f8a296a559c89aa91214a3575a9b
denise-lac <deniselacetera13@gmail.com>
2024-12-21 15:53:55 +0100

    Passo 2

--- TombolaNatalizia/src/App.java
+++ TombolaNatalizia/src/App.java
@@ -1,45 +1,58 @@
-/*Esercizio 2 - Tombola natalizia
-Scrivere un programma che simula il gioco della Tombola.
--Parte 1:
-    Estrarre casualmente una serie di numeri dall'1 al 90 (non si può estrarre 2 volte lo stesso numero)
-    Visualizzare i numeri estratti
-
--Parte 2:
-    Generare ulteriori 15 numeri dall'1 al 90 diversi tra loro (che rappresentano la scheda della tombola).
-    I numeri della scheda devono essere visualizzati sullo schermo appena parte il programma
-    mano a mano che i numeri della parte 1 vengono estratti, bisogna evidenziare i numeri della scheda che corrispondono (ad esempio racchiudendoli tra parentesi quadre)
-    il programma termina quando tutti i numeri della scheda sono stati estratti (Tombola!)
-
--Bonus (per chi termina prima del tempo assegnato) realizzare una o più delle seguenti funzionalità:
-    Gestire le cartelle multiple (chiedere all'utente quante cartelle vuole "comprare")
-    Visualizzare ogni cartella su 3 righe (come nella seguente immagine: https://www.bianconatale.com/wp-content/uploads/2011/12/Cartelle-Tombola-Bianco-Natale.jpg)
-    Visualizzare le vincite intermedie (ambo, terno, quaterna, cinquina)
-    Nella generazione della cartella seguire le seguenti regole:
-    Ogni riga deve contenere 5 numeri
-    In ogni colonna ci possono essere al massimo 2 numeri
-    La colonna in cui posizionare il numero dipende dal suo valore (da 1 a 9 nella prima colonna, da 10 a 19 nella seconda, ecc.)
-    La riga in cui posizionare il numero è casuale, ma in ogni colonna i numeri devono essere ordinati in maniera crescente */
-
-
 import java.util.ArrayList;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Random r = new Random();
         ArrayList<Integer> estrazione = new ArrayList<>();
-        Random r=new Random();
+        ArrayList<Integer> scheda = new ArrayList<>();
 
-        while(estrazione.size()<10) {
-            int numeri=r.nextInt(10)+1;
+        //Stampo un messaggio iniziale
+        System.out.println("GIOCHIAMO A TOMBOLA!");
+
+        //Genero randomicamente la scheda di 15 numeri
+        while (scheda.size() < 15) {
+            int numeriScheda = r.nextInt(90) + 1;
+            // mi assicuro che non ci siano doppioni
+            if (!scheda.contains(numeriScheda)) {
+                scheda.add(numeriScheda);
+            }
+        } 
+
+        // stampo la scheda iniziale
+        System.out.println("\n\nEcco la tua cartella:");
+        System.out.println("-------------------------------------------------------------------------------------------");
+        for (Integer i : scheda) {
+            System.out.print(i+ " ");
+        }
+        System.out.println("\n-------------------------------------------------------------------------------------------");
+
+        // Genero randomicamente 90 numeri
+        while (estrazione.size() < 90) {
+            int numeri = r.nextInt(90) + 1;
+
+            // mi assicuro che non ci siano doppioni
             if (!estrazione.contains(numeri)) {
                 estrazione.add(numeri);
+
+                // elimino i numeri estratti contenuti nella scheda
+                if (scheda.contains(numeri)) {
+                    System.out.println("\n\nDopo vari numeri estratti, finalmente ecco uno contenuto nella tua cartella:    "+numeri);
+                    System.out.println("\n\nCartella aggiornata:");
+                    System.out.println("-------------------------------------------------------------------------------------------");
+                    for (int n : scheda) {
+                        if (estrazione.contains(n)) {
+                            System.out.print(" ["+n+"] ");  // evidenzio i numeri estratti
+                        } else {
+                            System.out.print(n + " ");      // numeri non estratti
+                        }
+                    }
+                    System.out.println("\n-------------------------------------------------------------------------------------------");
+                }
             }
-            
+
         }
-        
-        for (Integer i : estrazione) {
-            System.out.println("estrazione "+i);
-        }
-        
+        System.out.println("\n\nTOMBOLA!\n\n");
+
     }
 }
